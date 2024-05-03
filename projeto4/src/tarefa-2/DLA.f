6738776a4f4083650c1d6f3b91de151aee6d6b57
jeftersantiago <jeftersmares@gmail.com>
2024-05-03 01:00:57 -0300

    DLA2D completo

--- projeto4/src/tarefa-2/DLA.f
+++ projeto4/src/tarefa-2/DLA.f
@@ -1,26 +1,24 @@
 !     Dinâmica DLA em 2D
+!     Para cada partícula:
+!     - Gera posicao inicial aleatoria.
+!     - Aplica dinâmica de random walk.
+!     - Ao agregar novas celulas armazena a posição e raio atual à um
+!     arquivo de dados.
 !     Parâmetros:
 !     Np -> numero de particulas
       subroutine DLA_2D(Np)
-      implicit integer (c-c, t-t, x-x, y-y)
-      parameter(N = 400)
+      implicit integer (x-x, y-y)
+      parameter(N = 500)
       dimension lattice(-N:N, -N:N)
-
-      dimension cell(2)
 
 !     semente inicial
       lattice(0, 0) = 1
-!     para cada partícula:
-!     - gera posicao inicial aleatoria
-!     - aplica dinâmica
-!     - ao fim da execucao salvamos toda grade.
-      
+     
       rnd = rand(iseed)
       
       R_in = 5.0
       R_f = 1.5 * R_in
 
-      open(3, file="trajetoria_celula.dat")
       open(2, file="particle_positions.dat")
 
       do i = 1, Np
@@ -34,32 +32,35 @@
 
          print *, "r = ", R_in
          s = 0
-         nx = abs(x)
-         ny = abs(y)
-
          touched = 0
 
-         do while(touched == 0 .and. nx < N .and. ny < N)
+         print *, "x = ", x
+         print *, "y = ", y
+
+         do while(touched == 0) 
 
             call random_step(x, y) 
 
-            print *, "x = ", x
-            print *, "y = ", y
-
             d = sqrt(real(x**2 + y ** 2))
             
+!     Captura celula ao redor da posição atual se houver
             do k = -1, 1
                do j = -1, 1
-                  s = s + lattice(x+k, y+j)
+!                Checagem de borda
+                  if(abs(x) < N .and. abs(y) < N)then
+                     s = s + lattice(x+k, y+j)
+                  end if
                end do
             end do
-
+!     Adiciona nova celula, atualiza raio
             if (d >= R_f) then
                touched = 1
             else if(s >= 1) then
                touched = 1
                lattice(x, y) = 1
+
                write(2, *) x, y, R_in
+
                if(d > R_in) then
                   R_in = d + 5
                   R_f = 1.5 * R_in
@@ -67,10 +68,7 @@
             end if
          end do
       end do
-
       close(2)
-      close(3)
-
       end subroutine DLA_2D
 
 !     Gera uma partícula em uma posicao aleatoria
