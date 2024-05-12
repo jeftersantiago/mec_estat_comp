9e22d1216881cd3b05b4e00cf3fc3a584257bc56
jeftersantiago <jeftersmares@gmail.com>
2024-05-12 14:46:28 -0300

    tarefa1 concluida - falta escrever

--- projeto5/src/tarefa-1/ising.f
+++ projeto5/src/tarefa-1/ising.f
@@ -1,96 +1,99 @@
         subroutine define_exponentials(exps, beta)
             dimension exps(-4:4)
             do i = -4,4
-                exps(i) = exp(-beta*i)/(exp(-beta*i)+exp(-beta*i)) 
+                exps(i) = exp(-beta*i * 1.0)! /(exp(-beta*i)+exp(-beta*i)) 
             end do  
 
         end subroutine define_exponentials
 
-        subroutine flip_spin(lattice, pbc, exps, H,  H_m)
-            implicit integer(p-p, s-s)
-            implicit real(J-J)
+        subroutine flip_spin(lattice, ipbc, exps, E,  m, L_real)
+            implicit integer(p-p, s-s, d-d)
+            ! implicit real(J-J)
             parameter(L = 100)
             parameter(J = 1.0)
             dimension exps(-4:4)
-            dimension lattice(1:L, 1:L)
-            dimension pbc(0:L+1)
+            byte lattice(1:L, 1:L)
+            dimension ipbc(0:L+1)
 
             ! choose a random site
-            i = floor(rand()* L) + 1
-            k = floor(rand()* L) + 1
+            i = floor(rand()* L_real) + 1
+            k = floor(rand()* L_real) + 1
 
             ! ∆M̃ = J[s(i − 1, j + s(i + 1, j) + s(i, j − 1) + s(i, j + 1)].
-            dM = lattice(pbc(i-1), k) + lattice(pbc(i+1), k)
-            dM = dM + lattice(i, pbc(k-1)) + lattice(i,pbc(k+1))
-
+            dM = lattice(ipbc(i-1), k) + lattice(ipbc(i+1), k)
+            dM = dM + lattice(i, ipbc(k-1)) + lattice(i,ipbc(k+1))
             dM = J * dM
 
             ! spin(i, j) and dM are positions of the lattice, so
             s = lattice(i, k)
-            e_flip = exps(floor(s*dM))/(exps(floor(-s*dM))+exps(floor(s*dM)))
+            e_flip = exps(s*dM)/(exps(-s*dM)+exps(s*dM))
 
-            if(rand() < res) then 
-                lattice(i, k) = -s
+            if(rand() < e_flip) then 
+                lattice(i, k) = -lattice(i, k)
                 ! Magnetization
-                H_m = H_m - 2 * s
+                m = m - 2 * s
                 ! Change in the energy
-                H = H - 2*s*dM
+                E = E - 2*s*dM
             end if  
-        end subroutine flip_spin
+         end subroutine flip_spin
 
-        function total_hamiltonian(lattice, pbc, m, L_real)
-            implicit integer(p-p)
-            implicit real(m-m)
+        function H_0(lattice, ipbc, m, L_real)
             parameter(L = 100)
-            dimension lattice(1:L, 1:L)
-            dimension pbc(0:L+1)
-            total_hamiltonian = 0
-            m = 0
+            byte lattice(1:L, 1:L)
+            dimension ipbc(0:L+1)
+            print *, "Computing total hamiltonian"
+            print *, "L_real = ", L_real
+            E_0 = 0
             do i = 1, L_real
-                do j = 1, L_real
-                    !   x axis
-                    adj = (lattice(pbc(i-1),j)+lattice(pbc(i+1), j))
-                    !   y axis
-                    adj = adj + (lattice(i, pbc(j-1)) + lattice(i, pbc(j+1)))
-                    total_hamiltonian = total_hamiltonian + lattice(i, j)*adj
+               do k = 1, L_real
+                   adj = lattice(ipbc(i-1),k)+lattice(ipbc(i+1),k)
+                   adj = adj+lattice(i,ipbc(k-1))+lattice(i,ipbc(k+1))
+                   E_0 = E_0 + adj * lattice(i, k)
+                   m = m + adj
+               end do
+            end do
+!           E = (-J/2) * (s(i, j)[s(i − 1, j) + s(i + 1, j) + s(i, j − 1) + s(i, j + 1))
+            H_0 = - 0.5 * E_0
+        end function H_0
 
-                    m = m + adj
-                end do  
-            end do  
-            total_hamiltonian = -(J / 2) * total_hamiltonian
-        end function total_hamiltonian
-
-        subroutine update_hamiltonian(lattice, pbc, i, k, H, J)
-            implicit integer(p-p)
+        subroutine update_hamiltonian(lattice, pbc, i, k, E, J)
+            implicit integer(p-p, a-a)
             implicit real(J-J)
             parameter(L = 100)
-            dimension lattice(1:L, 1:L)
+            byte lattice(1:L, 1:L)
             dimension pbc(0:L+1)
 
             adj = lattice(pbc(i-1),k) + lattice(pbc(i+1),k)
             adj = adj + lattice(i, pbc(k-1)) + lattice(i, pbc(k+1))
 
-            H = H - 2 * J * adj           
+            E = E - 2 * J * adj           
         end subroutine update_hamiltonian
 
-        !  conf = 0 -> ordered 
-        !  conf = 1 -> random
-        subroutine initialize_lattice(lattice, conf, L_real)
-            implicit integer (c-c)
-            parameter(L = 100)
-            dimension lattice(1:L, 1:L)
 
-            if (conf == 0) then 
-                do i = 1, L_real
-                    do j = 1, L_real
-                        lattice(i, j) = conf 
-                    end do  
-                end do  
-            else 
-                do i = 1, L_real
-                    do j = 1, L_real
-                        lattice(i, j) = rand() *  
-                    end do
-                end do
-            end if
+        subroutine initialize_lattice(lattice, m, L_real)
+            parameter(L = 100)
+            byte lattice(1:L, 1:L)
+            ! initializing lattice
+            m = 0
+            do i = 1, L_real
+               do k = 1, L_real
+                  lattice(i, k) = 1
+                  m = m + 1
+               end do
+            end do
         end subroutine initialize_lattice
+    
+        subroutine write_lattice(lattice, L_real, f_name)
+            implicit integer (f-f)
+            parameter(L = 100)
+            byte lattice(1:L, 1:L)
+            character *1 symb(-1:1)
+            symb(1) = '1'
+            symb(-1) = '0'
+        
+            do i = 1, L_real
+               write(f_name,'(100A2)') (symb(lattice(i,j)),j = 1,L_real)
+
+            end do  
+
+        end subroutine write_lattice
--- projeto5/src/tarefa-1/tarefa-1.f
+++ projeto5/src/tarefa-1/tarefa-1.f
@@ -1,64 +1,43 @@
-        implicit integer (p-p)
-        parameter(L = 60)
+        implicit real(j-j)
+        parameter(L = 100)
         dimension exps(-4:4)
-
-    !   PRESTAR ATENÇÃO A ISSO AQUI - PODE TA TUDO ERRADO
         byte lattice(1:L, 1:L)
 
         ! periodic boundary conditions
-        dimension pbc(0:L+1)
+        dimension ipbc(0:L+1)
         ! this or using mod
-        do i = 1, N
-            pbc(i) = i
+      
+        L_real = 100
+        
+        do i = 1, L_real
+            ipbc(i) = i
         end do  
-        pbc(0) = L
-        pbc(L+1) = 1
+        ipbc(0) = L_real
+        ipbc(L_real+1) = 1
 
         beta = 3
 
         call define_exponentials(exps, beta)
-        L_real = L
-        ! computes total energy and magnetization
+
         m = 0
-        E = total_hamiltonian(lattice, pbc, m, L_real)
+        call initialize_lattice(lattice, m, L_real)
+        
+        call write_lattice(lattice, L_real, 1)
 
-        call initialize_lattice(lattice, 1, L_real)
+         ! initial energy
+        E_0 = H_0(lattice, ipbc, m, L_real)
 
-        open(unit = 1, file="magnetizacao-temperatura.dat")
-        open(unit = 2, file=".dat")
-
-        ! monte carlo dynamics
-        do N = 1, 1000
-
-            
-
-
+        open(1, file="saida-configuracao.dat")
+        rnd = rand(iseed)
+         ! intialize monte carlo dynamics
+        do MC_step = 1, 1000
+            write(2, *) m, E
+            ! sweeps all configurations
+            ! randomly flips spins
+            do i = 1 , L_real * L_real
+               call flip_spin(lattice,ipbc,exps,E,m,L_real)
+            end do   
         end do  
+        call write_lattice(lattice, L_real, 1) 
+        close(1)
         end
-
-        subroutine write_lattice(lattice, L_real)
-            implicit integer (f-f)
-            parameter(L = 100)
-            byte lattice(1:L, 1:L)
-            character *1 symb(-1:1)
-            symb(1) = '+'
-            symb(-1) = '-'
-        
-            do i = 1, L_real
-                write(f_name, '(100A2)') (symb(lattice(i,j)), j = 1, L_real)
-                
-            end do  
-
-        end subroutine write_lattice
-        
-
-
-
-
-
-
-
-
-
-
-
